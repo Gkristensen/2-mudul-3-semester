755f8be89c0ed77e1b802da0c7f46856b9fb965f
Gabriel Kristersen <gabrielkristensen@gmail.com>
2016-09-21 20:43:02 +0200

    Maby dun
    Tror jeg har løst det er ikke helt sikker da class=curent ikke endre
    sig.

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 
 <ul>
-	<li class="<?php echo ($page == "p1" ? "active" : "")?>"><a href="p1.php">Page 1</a></li>
-	<li class="<?php echo ($page == "p2" ? "active" : "")?>"><a href="p2.php">Page 2</a></li>
-    <li class="<?php echo ($page == "p3" ? "active" : "")?>"><a href="p3.php">Page 3</a></li>
+	<li <?php if($current == 'p1.php') {echo 'class="current"';} ?>><a href="p1.php">Page 1</a></li>
+	<li <?php if($current == 'p2.php') {echo 'class="current"';} ?>><a href="p2.php">Page 2</a></li>
+    <li <?php if($current == 'p3.php') {echo 'class="current"';} ?>><a href="p3.php">Page 3</a></li>
 </ul>
 
