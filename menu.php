80b678dc2fd77c7a30132569ef37bb1e758bc59a
Gabriel Kristersen <gabrielkristensen@gmail.com>
2016-09-22 11:19:54 +0200

    menu php don
    menu virker og viser den aktive side.

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-
+<?php $current = basename($_SERVER['PHP_SELF']);?>
 <ul>
 	<li <?php if($current == 'p1.php') {echo 'class="current"';} ?>><a href="p1.php">Page 1</a></li>
 	<li <?php if($current == 'p2.php') {echo 'class="current"';} ?>><a href="p2.php">Page 2</a></li>
